00a7eced300602511ddb167836d0ccabc4408a12
mkrause <maikelkrause@gmail.com>
2018-10-14 23:05:15 +0200

    Update description of status.js.

--- src/status.js
+++ src/status.js
@@ -1,3 +1,4 @@
+// @flow
 
 /*
 A *status* is an object that describes the current loading state of some application state.
@@ -8,13 +9,13 @@ Status flags:
   - error: indicates that the last load attempt resulted in an error
 
 Note that each of the flags is independent, rather than being a linear transition (e.g. loading -> ready).
-This is so that we can (for example) start loading new data while keeping information such as an error
-state, for UI purposes.
+This is so that we can, for example, start loading new data while keeping information such as an error
+state, for UI purposes. All the possible combinations listed below.
 
 - !ready + !loading + !error     "pending", nothing done yet, not even attempted to load
 - !ready +  loading + !error     "loading", for the first time, no older data available
 -  ready + !loading + !error     "idle", can use the data as needed
--  ready +  loading + !error     "reloading", but there is data available in the meantime
+-  ready +  loading + !error     "reloading", loading but there is still data available to show
 - !ready + !loading +  error     failed, and not yet any attempt to retry (no older data available)
 - !ready +  loading +  error     failed, but we're currently retrying (no older data available)
 -  ready + !loading +  error     failed, and not yet any attempt to retry (there is older data available)
