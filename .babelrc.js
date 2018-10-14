8ed8651ae6d09225bd1deeec153afb7a97e0eaf0
mkrause <maikelkrause@gmail.com>
2018-10-14 23:04:52 +0200

    Update dependencies.

--- .babelrc.js
+++ .babelrc.js
@@ -20,8 +20,6 @@ module.exports = {
         }],
     ],
     plugins: [
-        // Note: this may cause issues with `export * from` syntax:
-        // https://github.com/babel/babel-loader/issues/195 (should be fixed in the latest version)
         // 'transform-runtime', // Needed to support generators
         
         '@babel/proposal-class-properties',
