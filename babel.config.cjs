1117d29e52ae207e49c8e0297ab8dd0ad54bab67
mkrause <maikelkrause@gmail.com>
2020-03-07 23:38:34 +0100

    Update babel config.

--- babel.config.cjs
+++ babel.config.cjs
@@ -6,12 +6,22 @@ module.exports = {
         '@babel/typescript',
         ['@babel/env', {
             targets: {
-                node: '6.9', // LTS (Boron)
-                browsers: ['>0.25%', 'not dead'],
+                browsers: [
+                    'node 8.9', // Support Node v8.9 LTS (Carbon)
+                    '>0.1%',
+                    'not dead',
+                    'not OperaMini all',
+                    'not IE < 11',
+                    'last 2 Edge versions',
+                ],
             },
             
             // Whether to transpile modules
             modules: env === 'cjs' ? 'commonjs' : false,
+            
+            // Do not include polyfills automatically. Leave it up to the consumer to include the right polyfills
+            // for their required environment.
+            useBuiltIns: false,
             
             exclude: [
                 // Do not transpile generators (saves us from needing a polyfill)
